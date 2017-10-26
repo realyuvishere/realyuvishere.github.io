2aab66daf302a11243bea61ae67a5d8fc8f1e70a
Yuv <billiondollarone07@gmail.com>
2017-10-26 14:01:33 +0530

    remove whole folder

--- Web_Development_Folder/schoolprojects/TISVV - SBv13.1/index.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function () {
-	document.title = "Google Presents";
-}, 1000);
-setTimeout(function () {
-	document.title = "Pixel 2";
-}, 2000);
-setTimeout(function () {
-	$(".logo").removeClass("tyl");
-}, 5100);
